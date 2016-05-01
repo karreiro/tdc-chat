09271836c7eb656e24a2c50cf83629f09fca58a6
Guilherme Carreiro <karreiro@gmail.com>
2016-05-01 19:40:08 -0300

    Add RoomsController#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :rooms, only: [:index]
+  resources :rooms, only: [:index, :show]
   resource :session, only: [:new, :create, :destroy]
 
   # Serve websocket cable requests in-process
--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -15,6 +15,20 @@ class RoomsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to new_session_url
   end
 
+  test 'should get show when an user is logged in' do
+    login('karreiro')
+
+    get room_url(rooms(:one))
+
+    assert_response :success
+  end
+
+  test 'should not get show when any user is logged in' do
+    get room_url(rooms(:one))
+
+    assert_redirected_to new_session_url
+  end
+
   def login(username)
     post session_url, params: { username: username }
   end
