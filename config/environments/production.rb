743801700eb1a30430180b101baad332b78adbd4
Guilherme Carreiro <karreiro@gmail.com>
2016-05-07 17:20:38 -0300

    WebSocket URL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://chat-tdc.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://chat-tdc.herokuapp.com', /http:\/\/chat-tdc.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
